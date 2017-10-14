1771580e6671654ba01c1f484c1723d6b9f038e8
Vincent Matthys <vmatthys@student.42.fr>
2017-10-14 20:51:26 +0200

    getClicks done

--- Panorama_Initial/Panorama.cpp
+++ Panorama_Initial/Panorama.cpp
@@ -12,45 +12,82 @@ using namespace Imagine;
 using namespace std;
 
 // Record clicks in two images, until right button click
-void getClicks(Window w1, Window w2, vector<IntPoint2>& pts1, vector<IntPoint2>& pts2)
+// Vector in std : http://fr.cppreference.com/w/cpp/container/vector
+void			getClicks(	Window w1,
+							Window w2,
+							vector<IntPoint2>& pts1,
+							vector<IntPoint2>& pts2)
 {
 	// ------------- TODO/A completer ----------
+	IntPoint2		p;
+	unsigned int	button;
+	int				window;
+
+	cout << "Select similar points in both windows." << endl;
+	cout << "Starting with the first window -- " << endl;
+	window = 1;
+	setActiveWindow(w1);
+	while ((button = getMouse(p)) != 3)
+	{
+		cout << "Pixel in window " << window << " in position " << p << endl;
+		if (window == 1)
+		{
+			window = 2;
+			pts1.push_back(p);
+			setActiveWindow(w2);
+		}
+		else
+		{
+			window = 1;
+			pts2.push_back(p);
+			setActiveWindow(w1);
+		}
+	}
+	cout << "Number of points selected : " << pts1.size() << endl;
+	if (pts2.size() < 4)
+		cerr << "Wrong number of points selected" << endl;
+	else if (pts1.size() != pts2.size())
+		cerr << "Missing the last similar point in window 2" << endl;
 }
 
 // Return homography compatible with point matches
-Matrix<float> getHomography(const vector<IntPoint2>& pts1,
-                            const vector<IntPoint2>& pts2) {
-    size_t n = min(pts1.size(), pts2.size());
-    if(n<4) {
-        cout << "Not enough correspondences: " << n << endl;
-        return Matrix<float>::Identity(3);
-    }
-    Matrix<double> A(2*n,8);
-    Vector<double> B(2*n);
+Matrix<float>	getHomography(	const vector<IntPoint2>& pts1,
+								const vector<IntPoint2>& pts2)
+{
+	size_t n = min(pts1.size(), pts2.size());
+
+	if (n < 4)
+	{
+		cout << "Not enough correspondences: " << n << endl;
+		return Matrix<float>::Identity(3);
+	}
+	Matrix<double> A(2 * n, 8);
+	Vector<double> B(2 * n);
     // ------------- TODO/A completer ----------
 
-    B = linSolve(A, B);
-    Matrix<float> H(3, 3);
-    H(0,0)=B[0]; H(0,1)=B[1]; H(0,2)=B[2];
-    H(1,0)=B[3]; H(1,1)=B[4]; H(1,2)=B[5];
-    H(2,0)=B[6]; H(2,1)=B[7]; H(2,2)=1;
+	B = linSolve(A, B);
+	Matrix<float> H(3, 3);
+	H(0, 0) = B[0]; H(0, 1) = B[1]; H(0, 2) = B[2];
+	H(1, 0) = B[3]; H(1, 1) = B[4]; H(1, 2) = B[5];
+	H(2, 0) = B[6]; H(2, 1) = B[7]; H(2, 2) = 1;
 
-    // Sanity check
-    for(size_t i=0; i<n; i++) {
-        float v1[]={(float)pts1[i].x(), (float)pts1[i].y(), 1.0f};
-        float v2[]={(float)pts2[i].x(), (float)pts2[i].y(), 1.0f};
-        Vector<float> x1(v1,3);
-        Vector<float> x2(v2,3);
-        x1 = H*x1;
-        cout << x1[1]*x2[2]-x1[2]*x2[1] << ' '
-             << x1[2]*x2[0]-x1[0]*x2[2] << ' '
-             << x1[0]*x2[1]-x1[1]*x2[0] << endl;
-    }
-    return H;
+	// Sanity check
+	for(size_t i=0; i<n; i++)
+	{
+		float v1[]={(float)pts1[i].x(), (float)pts1[i].y(), 1.0f};
+		float v2[]={(float)pts2[i].x(), (float)pts2[i].y(), 1.0f};
+		Vector<float> x1(v1,3);
+		Vector<float> x2(v2,3);
+		x1 = H*x1;
+		cout	<< x1[1]*x2[2]-x1[2]*x2[1] << ' '
+				<< x1[2]*x2[0]-x1[0]*x2[2] << ' '
+				<< x1[0]*x2[1]-x1[1]*x2[0] << endl;
+	}
+	return H;
 }
 
 // Grow rectangle of corners (x0,y0) and (x1,y1) to include (x,y)
-void growTo(float& x0, float& y0, float& x1, float& y1, float x, float y) {
+void			growTo(float& x0, float& y0, float& x1, float& y1, float x, float y) {
     if(x<x0) x0=x;
     if(x>x1) x1=x;
     if(y<y0) y0=y;
@@ -58,7 +95,7 @@ void growTo(float& x0, float& y0, float& x1, float& y1, float x, float y) {
 }
 
 // Panorama construction
-void panorama(const Image<Color,2>& I1, const Image<Color,2>& I2,
+void			panorama(const Image<Color,2>& I1, const Image<Color,2>& I2,
               Matrix<float> H) {
     Vector<float> v(3);
     float x0=0, y0=0, x1=I2.width(), y1=I2.height();
@@ -89,42 +126,43 @@ void panorama(const Image<Color,2>& I1, const Image<Color,2>& I2,
 }
 
 // Main function
-int main(int argc, char* argv[]) {
-    const char* s1 = argc>1? argv[1]: srcPath("image0006.jpg");
-    const char* s2 = argc>2? argv[2]: srcPath("image0007.jpg");
+int				main(int argc, char** argv)
+{
+	const char* s1 = argc>1 ? argv[1] : srcPath("image0006.jpg");
+	const char* s2 = argc>2 ? argv[2] : srcPath("image0007.jpg");
 
-    // Load and display images
-    Image<Color> I1, I2;
-    if( ! load(I1, s1) ||
-        ! load(I2, s2) ) {
-        cerr<< "Unable to load the images" << endl;
-        return 1;
-    }
-    Window w1 = openWindow(I1.width(), I1.height(), s1);
-    display(I1,0,0);
-    Window w2 = openWindow(I2.width(), I2.height(), s2);
-    setActiveWindow(w2);
-    display(I2,0,0);
+	// Load and display images
+	Image<Color> I1, I2;
+	if(!load(I1, s1) || !load(I2, s2))
+	{
+		cerr<< "Unable to load the images" << endl;
+		return 1;
+	}
+	Window w1 = openWindow(I1.width(), I1.height(), s1);
+	display(I1, 0, 0);
+	Window w2 = openWindow(I2.width(), I2.height(), s2);
+	setActiveWindow(w2);
+	display(I2, 0, 0);
 
-    // Get user's clicks in images
-    vector<IntPoint2> pts1, pts2;
-    getClicks(w1, w2, pts1, pts2);
+	// Get user's clicks in images
+	vector<IntPoint2> pts1, pts2;
+	getClicks(w1, w2, pts1, pts2);
 
-    vector<IntPoint2>::const_iterator it;
-    cout << "pts1="<<endl;
-    for(it=pts1.begin(); it != pts1.end(); it++)
-        cout << *it << endl;
-    cout << "pts2="<<endl;
-    for(it=pts2.begin(); it != pts2.end(); it++)
-        cout << *it << endl;
+	vector<IntPoint2>::const_iterator it;
+	cout << "pts1=" << endl;
+	for(it = pts1.begin(); it != pts1.end(); it++)
+		cout << *it << endl;
+	cout << "pts2=" << endl;
+	for(it = pts2.begin(); it != pts2.end(); it++)
+		cout << *it << endl;
 
-    // Compute homography
-    Matrix<float> H = getHomography(pts1, pts2);
-    cout << "H=" << H/H(2,2);
+	// Compute homography
+	Matrix<float> H = getHomography(pts1, pts2);
+	cout << "H=" << H/H(2,2);
 
-    // Apply homography
-    panorama(I1, I2, H);
+	// Apply homography
+	panorama(I1, I2, H);
 
-    endGraphics();
-    return 0;
+	endGraphics();
+	return (0);
 }
